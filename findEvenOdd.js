7e91958562b7bc5dd28a963c8c500f9b760d6719
Simar Rajput <37287596+Simarrajput12@users.noreply.github.com>
2023-12-26 13:08:39 +0530

    find even and odd number

---
+++ findEvenOdd.js
@@ -0,0 +1,14 @@
+
+  // TODO document why this function 'findEvenOdd' is empty
+
+function findEvenOdd (num){
+ if(num % 2 === 0){
+    return 'The number is Even'
+ }
+ else {
+    return 'The number is Odd'
+ }
+}
+
+
+console.log(findEvenOdd(4))
