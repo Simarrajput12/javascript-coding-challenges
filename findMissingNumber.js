bb5d8c0597a09157aa681c45edbdbbb169c75579
Simar Rajput <37287596+Simarrajput12@users.noreply.github.com>
2025-03-13 18:30:47 +0530

    find missing number

--- findMissingNumber.js
+++ findMissingNumber.js
@@ -2,10 +2,8 @@
 
 function findMissingNumber(arr, n) {
     let total = (n * (n + 1)) / 2; // Sum of first N numbers
-    console.log("Total", total);
     let sumArr = arr.reduce((acc, num) => acc + num, 0);
-    console.log("sum", sumArr);
-    // return total - sumArr;
+    return `Missing number is ${total - sumArr}`;
 }
 
 console.log(findMissingNumber([1, 2, 4, 5, 6], 6)); // Output: 3
